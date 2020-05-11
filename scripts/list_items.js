e504237557dc6c9247728674319f1c4aedea6e6f
Bodnárné Kate <bodnarnekate@gmail.com>
2020-05-11 18:41:59 +0200

    Kiválasztott légitársaság és index transition

--- scripts/list_items.js
+++ scripts/list_items.js
@@ -1,25 +1,27 @@
 // lista elemek DINAMIC feltöltése
 function listItemFeltolt() {
     
-/* ---   próba adatok   --------------*/
-var tomb=["HTML", "CSS", "JavaScript"];
-/* ---------------------------------- */
-
-var text="";
-var most="";
-var i;
-
-for (i = 0; i < tomb.length; i++) {
+  sessionStorage.clear();  
     
-    /*-- itt link volt --
-    text=text+"<a href=\"legitars.html\" class=\"list-group-item list-group-item-action\" id=\""+azon+"\">"+tomb[i]+"</a>";*/
-    
-    //-- de lecserélem buttonra --
-    most=" type=\"button\" class=\"list-group-item list-group-item-action\" onclick=\"melyik(this)\">"+tomb[i]+"</button>";
-    
-    text=text+"<button"+most;
+  /* ---   próba adatok tömbben  -------*/
+  var tomb=["HTML", "CSS", "JavaScript"];
+  /* ---------------------------------- */
 
-}
+  var text="";
+  var most="";
+  var i;
+
+  for (i = 0; i < tomb.length; i++) {
+    
+     /*-- itt link volt --
+     text=text+"<a href=\"legitars.html\" class=\"list-group-item list-group-item-action\" id=\""+azon+"\">"+tomb[i]+"</a>";*/
+    
+     //-- de lecserélem buttonra --
+     most=" type=\"button\" class=\"list-group-item list-group-item-action\" onclick=\"melyik(this)\">"+tomb[i]+"</button>";
+    
+     text=text+"<button"+most;
+
+  };
 
 document.getElementById("gombok").innerHTML=text;
 
@@ -28,13 +30,33 @@ document.getElementById("gombok").innerHTML=text;
 // melyik légitársaság lett kiválasztva a listából? lista.html + paraméter-átadás
 function melyik(elmnt) {
     var tartalom=elmnt.textContent;
- 
+    
     sessionStorage.setItem("data", tartalom);        //paraméter
     document.location ="../pages/legitars.html";
+    
+}
+
+//teszt-hez képek cseréje
+  var kepek = [];
+  kepek['HTML'] = 'figure-974810_640.jpg';
+  kepek['CSS'] = 'figure-974816_640.jpg';
+  kepek['JavaScript'] = 'smurf-5009586_640.png';
+
+function mutatKep(oldal) {
+    
+    document.getElementById('logo').src = '../images/'+kepek[oldal];
+    document.getElementById('logo').title = oldal;
 
 }
+//--- eddig tesztadat a légitárs logójával ;-)
+
+
 // kiválasztott légitársaság nevét, képét frissíti a legitars.html oldalon
 function mutat() {
     document.getElementById("data").innerHTML=sessionStorage.getItem("data");
     document.getElementById("neve").innerHTML=sessionStorage.getItem("data");
+   
+    mutatKep(sessionStorage.getItem("data"));
+    
 }
+
